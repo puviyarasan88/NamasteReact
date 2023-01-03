dbffb4c969eabe4a8ea899d40c8fc38ee71f6cb1
puvi <puviarasan88@gmail.com>
2023-01-03 04:31:14 -0500

    Updated latest code

--- Chapter2_IgniteApp/app.js
+++ Chapter2_IgniteApp/app.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-
-
 const heading = React.createElement ("h1",{ id : "heading",class:"headingClass"},"Heading 1");
 const heading1 = React.createElement("h2", {id: "heading1", class:"headingClass"}, "Heading 2");
 
